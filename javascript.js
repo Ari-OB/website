6ed145b1e598449c4e36a757ce9628fff948c312
Ari-OB <ariadneolmob@gmail.com>
2025-08-31 17:54:47 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function submit(){
+    var nombre=document.getElementById("nombre").value;
+    var correo=document.getElementById("correo").value;
+    var mensaje=document.getElementById("mensaje").value;
+
+    console.log(nombre + " " + correo + " " + mensaje);
+}
