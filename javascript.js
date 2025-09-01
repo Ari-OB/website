5c6bbc45b5c748b1d038c5d78a7b66ec08ad653a
Ari-OB <ariadneolmob@gmail.com>
2025-08-31 20:03:14 -0400

    Update and rename javascript.js to script.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-function submit(){
-    var nombre=document.getElementById("nombre").value;
-    var correo=document.getElementById("correo").value;
-    var mensaje=document.getElementById("mensaje").value;
-
-    console.log(nombre + " " + correo + " " + mensaje);
-}
